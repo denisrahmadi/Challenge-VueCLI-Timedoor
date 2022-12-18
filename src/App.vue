751fc276105f4b8038f94bf6cc36be0e0502fe90
Denis Rahmadi <93427155+denisrahmadi@users.noreply.github.com>
2022-12-18 15:01:05 +0700

    Latihan 3 [6]Membuat Props

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div>
     <app-header></app-header>
     <main>
@@ -7,22 +7,10 @@
           <h1>NOW SHOWING IN CINEMAS</h1>
         </div>
         <div class="movies">
-          <div class="movie-detail">
-            <img src="./assets/movie1.jpg" alt="Movie">
-            <p>INANG</p>
-          </div>
-          <div class="movie-detail">
-            <img src="./assets/movie2.jpg" alt="Movie">
-            <p>KALIAN PANTAS MATI</p>
-          </div>
-          <div class="movie-detail">
-            <img src="./assets/movie3.jpg" alt="Movie">
-            <p>HALLOWEEN ENDS</p>
-          </div>
-          <div class="movie-detail">
-            <img src="./assets/movie4.jpg" alt="Movie">
-            <p>PAMALI</p>
-          </div>
+          <app-movie 
+              v-for="movie in movies" :key="movie.id" 
+              :movie="movie"
+          ></app-movie>
         </div>
       </div>
     </main>
@@ -31,15 +19,27 @@
 </template>
 
 <script>
-  import HeaderMain from "./components/HeaderMain.vue";
-  import FooterMain from "./components/FooterMain.vue";
-  export default {
-    components: {
-      "app-header" : HeaderMain,
-      "app-footer" : FooterMain,
+import HeaderMain from "./components/HeaderMain.vue";
+import FooterMain from "./components/FooterMain.vue";
+import MovieMain from "./components/MovieMain.vue";
 
+export default {
+  data() {
+    return {
+      movies: [
+        { id: 1, title: "INANG", imageName: "movie1.jpg" },
+        { id: 2, title: "KALIAN PANTAS MATI", imageName: "movie2.jpg" },
+        { id: 3, title: "HALOWEEN ENDS", imageName: "movie3.jpg" },
+        { id: 4, title: "PAMALI", imageName: "movie4.jpg" },
+      ],
     }
-  }
+  },
+  components: {
+    "app-header": HeaderMain,
+    "app-footer": FooterMain,
+    "app-movie": MovieMain,
+  },
+};
 </script>
 
 <style>
@@ -66,7 +66,82 @@ header {
 
 /* Header */
 
+/* Now Showing */
+.movies {
+  display: flex;
+  justify-content: center;
+}
 
+.title h1 {
+  font-size: 25px;
+}
+/* Footer */
+</style> -->
+
+<template>
+  <div>
+    <app-header></app-header>
+    <main>
+      <div class="container">
+        <div class="title">
+          <h1>NOW SHOWING IN CINEMAS</h1>
+        </div>
+        <div class="movies">
+          <app-movie v-for="data in dataMovie" :key="data.id" :movie="data"></app-movie>
+        </div>
+      </div>
+    </main>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import HeaderMain from "./components/HeaderMain.vue";
+import FooterMain from "./components/FooterMain.vue";
+import MovieMain from "./components/MovieMain.vue";
+
+export default {
+  data() {
+    return {
+      dataMovie: [
+        { id: 1, title: "INANG", imageName: "movie1.jpg" },
+        { id: 2, title: "KALIAN PANTAS MATI", imageName: "movie2.jpg" },
+        { id: 3, title: "HALOWEEN ENDS", imageName: "movie3.jpg" },
+        { id: 4, title: "PAMALI", imageName: "movie4.jpg" },
+      ],
+    };
+  },
+  components: {
+    "app-header": HeaderMain,
+    "app-footer": FooterMain,
+    "app-movie": MovieMain,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0px;
+  font-family: "Roboto", sans-serif;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+header {
+  position: sticky;
+  top: 0px;
+  background-color: white;
+}
+
+.container {
+  margin-left: 200px;
+  margin-right: 200px;
+}
+
+/* Header */
 
 /* Now Showing */
 .movies {
@@ -74,25 +149,8 @@ header {
   justify-content: center;
 }
 
-.movie-detail img {
-  border-radius: 20px;
-  width: 230px;
-  height: 350px;
-}
-
-.movie-detail {
-  margin: 7px;
-}
-
-.movie-detail p {
-  margin-top: 15px;
-  font-weight: 1000;
-}
-
 .title h1 {
   font-size: 25px;
 }
-
 /* Footer */
-
 </style>
---
+++ src/components/MovieMain.vue
@@ -0,0 +1,74 @@
+<!-- <template>
+  <div>
+    <div class="movie-detail">
+      <img :src="movieImage" alt="Movie" />
+      <p>{{ movie.title }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["movie"],
+  computed: {
+    movieImage(){
+      return require(`../assets/${this.movie.imageName}`)
+    }
+  }
+};
+</script>
+
+<style>
+.movie-detail img {
+  border-radius: 20px;
+  width: 230px;
+  height: 350px;
+}
+
+.movie-detail {
+  margin: 7px;
+}
+
+.movie-detail p {
+  margin-top: 15px;
+  font-weight: 1000;
+}
+</style> -->
+
+<template>
+  <div>
+    <div class="movie-detail">
+      <img :src="movieImage" alt="Movie" />
+      <p>{{ movieTitle }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["movie"],
+  data() {
+    return {
+      movieTitle: this.movie.title,
+      movieImage: require(`../assets/${this.movie.imageName}`)
+    }
+  },
+};
+</script>
+
+<style>
+.movie-detail img {
+  border-radius: 20px;
+  width: 230px;
+  height: 350px;
+}
+
+.movie-detail {
+  margin: 7px;
+}
+
+.movie-detail p {
+  margin-top: 15px;
+  font-weight: 1000;
+}
+</style>
